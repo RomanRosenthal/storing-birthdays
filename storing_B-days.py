3a38be31ba40ecbee273ac75f801e6709eab76f5
RomanRosenthal <jamielrosenthal@gmail.com>
2026-09-22 16:47:42 -0500

    Add files via upload

---
+++ storing_B-days.py
@@ -0,0 +1,13 @@
+print("a boy named raj is wanting to figure out his 5 friends birthdays")
+
+friend1 = "September 1st"
+friend2 = "October 15th"
+friend3 = "December 25th"
+friend4 = "January 10th"
+friend5 = "March 3rd"
+
+print("freind 1's birthday is on", friend1)
+print("freind 2's birthday is on", friend2)
+print("freind 3's birthday is on", friend3)
+print("freind 4's birthday is on", friend4)
+print("freind 5's birthday is on", friend5)
